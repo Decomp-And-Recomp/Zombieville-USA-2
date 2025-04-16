6d0a7f01190f2159486f5cb5ca71de9219bc57e8
Joe Rice <joerice123455t@gmail.com>
2025-04-16 15:20:14 +0100

    mouse lock and 1 2 3 to change weapons

---
+++ Assets/MouseLock.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+    void Awake()
+    {
+        // Prevent this object from being destroyed on scene load
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        // Check if F1 key is pressed
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            // Toggle cursor lock state
+            if (Cursor.lockState == CursorLockMode.None)
+            {
+                // Lock the cursor to the center of the screen
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false; // Make the cursor invisible
+            }
+            else
+            {
+                // Unlock the cursor and make it visible
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
